a48993a54f0d8f04734bc19fcfacd0887523fd11
Jian-Long Huang <contact@jlhg.org>
2023-08-23 18:13:01 +0800

    Fix: Could not find gem 'config (= 4.2.0)'

--- gem/config.rb
+++ gem/config.rb
@@ -1,7 +1,7 @@
 # Easiest way to add multi-environment yaml settings to Rails,
 # Sinatra, Pandrino and other ruby projects.
 # https://github.com/railsconfig/config
-gem "config", "4.1.0", github: "rubyconfig/config"
+gem "config", "4.2.1", github: "rubyconfig/config"
 run "bundle install"
 run "rails g config:install"
 inside("config/initializers") do
